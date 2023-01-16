616ee860b8086dbad6dc58410b013030a7fa51d1
Royson Gonsalves <royson.g@talentinc.com>
2023-01-16 15:13:54 +0530

    first commit

---
+++ priv/repo/migrations/20230112220609_create_user.exs
@@ -0,0 +1,19 @@
+defmodule First.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def up do
+    create table("weather") do
+      add :city,    :string, size: 40
+      add :temp_lo, :integer
+      add :temp_hi, :integer
+      add :prcp,    :float
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table("weather")
+  end
+
+end
